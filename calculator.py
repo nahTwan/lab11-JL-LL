76af0d6b9f06ac99955113ec54b018a12b4ec421
Jordon Lawson <jordonlawson@Jordons-MacBook-Pro.local>
2026-04-08 14:41:55 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,29 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+import math
 
+def add(a, b):
+     return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 0
+
+def logarithm(a, b):
+    try:
+        return math.log(a,b)
+    except ValueError:
+        return 0
+
+def exponent(a, b):
+    return a * b
 
