f35bf6bf876d2459004061b407905930d9a40d4e
lucas-leinweber <lucasleinweber06@gmail.com>
2026-04-08 15:48:13 -0400

    Update calculator.py shorten names

--- calculator.py
+++ calculator.py
@@ -25,10 +25,10 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     try:
         return a/b
     except ZeroDivisionError:
@@ -41,6 +41,6 @@ def logarithm(a, b):
         raise ValueError("Invalid argument")
     return math.log(a, b)
 
-def exponent(a, b):
+def exp(a, b):
     return pow(a, b)
 
